f04ce96195583ecda2b19bb91ced8ecd76ad3442
coder-dom <giangtruong100702@gmail.com>
2024-10-08 14:50:44 +0700

    render ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -5,10 +5,26 @@
  * @author      GIANG TRƯỜNG
  */
 
+/** model */
+const Product = require('../../models/product.model');
+
 // [GET] /admin/dashboard
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
     try{
-        res.send("PRODUCT")
+        // object tìm kiếm trong database
+        const findDb = {
+            status: "active",
+            deleted: false
+        }
+
+        // Lấy dữ liệu từ database
+        let records = await Product.find({});
+        
+
+        res.render("admin/pages/products/index", {
+            title: `Danh sách sản phẩm`,
+            records
+        });
     }
     catch(error) {
         console.error("Error Product", error);
---
+++ models/product.model.js
@@ -0,0 +1,37 @@
+/**
+ * @description MODEL PRODUCT
+ * @exports     MODEL
+ * @author      GIANG TRƯỜNG
+ */
+
+/** import & require package */
+const mongoose = require('mongoose');
+
+/** define schema */
+const productSchema = mongoose.Schema(
+    {
+        title:  String,     
+        slug: String,       // slug
+        author: String,     // id của model author
+        category: String,   // id của model category
+        thumbnail: String,  // link ảnh
+        description: String,
+        price: Number,
+        discountPercentage: Number,
+        stock: Number,
+        sold:  Number,      // đã bán được ...
+        position: Number,
+        status: String,
+        deleted: {
+            type: Boolean,
+            default: false
+        },
+        featured: Boolean   // nổi bật
+    }
+);
+
+/** create model */
+const Product = mongoose.model('Product', productSchema, 'products');
+
+/** exports */
+module.exports = Product;
